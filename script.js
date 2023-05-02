7bb0b7f8b47240f4c2c67c85be41e21e7c6e2d3c
implObserver <prognozman@mail.ru>
2023-05-02 13:33:55 +0500

    feat:add logic for one round

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+let computerSelection = getComputerChoise();
+
+console.log(playOneRound(computerSelection, playerSelection));
 
 function getComputerChoise() {
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -15,5 +18,28 @@ function describeTheChoice(choice) {
             return 'Scissors';
         default:
             break;
+    }
+}
+
+function playOneRound(computerSelection, playerSelection) {
+    return compareSelections(computerSelection, playerSelection);
+}
+
+function compareSelections(computerSelection, playerSelection) {
+    switch (computerSelection + playerSelection) {
+        case 'Rockpaper':
+            return `You Win! Paper beats ${computerSelection}`;
+        case 'Rockscissors':
+            return `You Lose! ${computerSelection} beats Scissors`;
+        case 'Paperrock':
+            return `You Lose! ${computerSelection} beats Rock`;
+        case 'Paperscissors':
+            return `You Win! Scissors beats ${computerSelection}`;
+        case 'Scissorsrock':
+            return `You Win! Rock beats ${computerSelection}`;
+        case 'Scissorspaper':
+            return `You Lose! ${computerSelection} beats Papper`;
+        default:
+            return 'Round result : draw';
     }
 }
