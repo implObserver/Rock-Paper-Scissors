8b3d288ac01d5c893fe148a47a13151d6787c6d6
implObserver <prognozman@mail.ru>
2023-05-02 12:51:09 +0500

    feat:add logic for computer choise

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoise() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    return describeTheChoice(choice);
+}
+
+function describeTheChoice(choice) {
+    switch (choice) {
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper';
+        case 3:
+            return 'Scissors';
+        default:
+            break;
+    }
+}
