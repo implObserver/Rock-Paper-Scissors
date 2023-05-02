0e3f5bdba7420a56773eefd45d09a2c1904c11fa
implObserver <prognozman@mail.ru>
2023-05-02 12:59:41 +0500

    feat:add choise for player

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+
 function getComputerChoise() {
     let choice = Math.floor(Math.random() * 3) + 1;
     return describeTheChoice(choice);
